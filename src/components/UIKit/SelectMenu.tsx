dce21af655809eef3a72d7b27d9da81c72a544f6
Afiz Savage <savageafiz9@gmail.com>
2025-08-13 16:36:01 +0000

    increase waitlist-form input fields sizes on mobile devices

--- src/components/UIKit/SelectMenu.tsx
+++ src/components/UIKit/SelectMenu.tsx
@@ -23,7 +23,7 @@ export default function SelectMenu({ options, selected, setSelected }: SelectMen
         <Listbox value={selected} onChange={setSelected} >
             <Label className="block text-sm/6 font-medium text-gray-900 dark:text-white">School Level</Label>
             <div className="relative mt-3">
-                <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:focus-visible:outline-indigo-500 after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-transparent after:ring-inset sm:focus-within:after:ring-2 sm:focus-within:after:ring-zinc-800">
+                <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-2 sm:py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:focus-visible:outline-indigo-500 after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-transparent after:ring-inset sm:focus-within:after:ring-2 sm:focus-within:after:ring-zinc-800">
                     <span className="col-start-1 row-start-1 truncate pr-6">{selected.name}</span>
                     <ChevronUpDownIcon
                         aria-hidden="true"
