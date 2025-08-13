053a977cfc0e5db87e0692abfbc627381bc1ff48
Afiz Savage <savageafiz9@gmail.com>
2025-08-13 18:09:29 +0000

    fix select-menu button styes

--- src/components/UIKit/SelectMenu.tsx
+++ src/components/UIKit/SelectMenu.tsx
@@ -3,8 +3,7 @@
 import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
 import { ChevronUpDownIcon } from '@heroicons/react/16/solid'
 import { CheckIcon } from '@heroicons/react/20/solid'
-
-
+import clsx from 'clsx'
 
 type Option = {
     id: number, name: string
@@ -23,13 +22,25 @@ export default function SelectMenu({ options, selected, setSelected }: SelectMen
         <Listbox value={selected} onChange={setSelected} >
             <Label className="block text-sm/6 font-medium text-gray-900 dark:text-white">School Level</Label>
             <div className="relative mt-3">
-                <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-2 sm:py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:focus-visible:outline-indigo-500 after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-transparent after:ring-inset sm:focus-within:after:ring-2 sm:focus-within:after:ring-zinc-800">
-                    <span className="col-start-1 row-start-1 truncate pr-6">{selected.name}</span>
+                <ListboxButton
+                    className={clsx(
+                        'relative block w-full appearance-none rounded-lg text-left',
+                        'px-[calc(--spacing(3.5)-1px)] py-[calc(--spacing(2)-1px)]',
+                        'sm:px-[calc(--spacing(3)-1px)] sm:py-[calc(--spacing(1.5)-1px)]',
+                        'text-base/6 text-zinc-950 placeholder:text-zinc-400 sm:text-sm/6',
+                        'border border-zinc-950/15 data-hover:border-zinc-950/20',
+                        'bg-transparent focus:outline-hidden',
+                        'data-invalid:border-red-500 data-invalid:data-hover:border-red-500',
+                        'data-disabled:border-zinc-950/20 shadow-xs'
+                    )}
+                >
+                    <span className="truncate pr-6">{selected?.name}</span>
                     <ChevronUpDownIcon
                         aria-hidden="true"
-                        className="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4 dark:text-gray-400"
+                        className="absolute right-3 top-1/2 size-5 -translate-y-1/2 text-gray-500 sm:size-4 dark:text-gray-400"
                     />
                 </ListboxButton>
+
 
                 <ListboxOptions
                     transition
@@ -39,11 +50,11 @@ export default function SelectMenu({ options, selected, setSelected }: SelectMen
                         <ListboxOption
                             key={option.id}
                             value={option}
-                            className="group relative cursor-default py-2 pr-9 pl-3 text-gray-900 select-none data-focus:bg-indigo-600 data-focus:text-white data-focus:outline-hidden dark:text-white dark:data-focus:bg-indigo-500"
+                            className="group relative cursor-default py-2 pr-9 pl-3 text-gray-900 select-none data-focus:bg-primary-600 data-focus:text-white data-focus:outline-hidden dark:text-white dark:data-focus:bg-primary-500"
                         >
                             <span className="block truncate font-normal group-data-selected:font-semibold">{option.name}</span>
 
-                            <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-not-data-selected:hidden group-data-focus:text-white dark:text-indigo-400">
+                            <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-primary-600 group-not-data-selected:hidden group-data-focus:text-white dark:text-primary-400">
                                 <CheckIcon aria-hidden="true" className="size-5" />
                             </span>
                         </ListboxOption>
