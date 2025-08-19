a3bdb8fa902d15106249f132974d901cf84297e5
Afiz Savage <savageafiz9@gmail.com>
2025-08-19 10:45:50 +0000

    shave off dashboard components

--- src/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from '@/components/Dashboard/DashboardLayout';
-
-export default function AdminLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <DashboardLayout>{children}</DashboardLayout>;
-} 
